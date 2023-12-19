085fb8a850fa3884305f90b9b5a078cd74f1b408
Krzysztof Blicharz <19705@student.ans-elblag.pl>
2023-12-19 11:50:53 +0100

    UI Created

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,10 +13,12 @@ namespace CodeQuotes
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("ChakraPetch-Regular.ttf", "ChakraPetchRegular");
+                    fonts.AddFont("ChakraPetch-Bold.ttf", "ChakraPetchBold");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
